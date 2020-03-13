3740c2107e21449fcd514a108f475438e3cbd171
light604 <sakuya604@gmail.com>
2020-03-13 21:58:22 +0800

    0313

--- app.py
+++ app.py
@@ -187,10 +187,10 @@ def search(event):
     else:
         for x in range(0,len(r),10):
             readresult = "OrderID:"+r[x]+"\n地區:"+r[x+1]+"\n外送者:"+r[x+2]+"\n使用者:"+r[x+3]+"\n收單時間:"+r[x+4]+"\n送達時間:"+r[x+5]+"\n上限份數:"+r[x+6]+"\n訂單明細:"+r[x+7]+"\n目前總份數:"+r[x+8]+"\n取貨地點:"+r[x+9]
-            line_bot_api.push_message(U_ID,TextMessage(text = "查詢結果\n"+readresult))
+            line_bot_api.push_message("Cd495babd31cff04b3743958031d8dd71",TextMessage(text = "查詢結果\n"+readresult))
 
 def delete_ex(event):
-    ID = event.source.user_id
+    #ID = event.source.user_id
     line_bot_api.push_message("Cd495babd31cff04b3743958031d8dd71",TextMessage(text= "請輸入要刪除的訂單編號，僅有使用者可以刪除與自已相關的訂單，以下是範例"))
     line_bot_api.reply_message(event.reply_token,TextMessage(text = "刪除訂單編號:0"))
 
